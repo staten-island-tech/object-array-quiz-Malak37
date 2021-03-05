b2e4e76ae6b30c5ca7fda005724839990c63d9b7
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-03-05 10:59:38 -0500

    commit for now

--- app.js
+++ app.js
@@ -15,23 +15,52 @@ const presidents = [
 
 //Challenge Questions
 //1) Filter all presidents, leaving only the Democratic ones
-
-/*const democratic = presidents.filter((president) => (president.party = "R"));
-console.log(democratic);*/
-const democratic = presidents.filter(function (president){
-  if (president.party = "D")
-  {
-    return true;
-  }
-  } 
-
-
-
+const democratic = presidents.filter((president) => president.party === "D");
+console.table(democratic);
 
 //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+const republican = presidents.filter(
+  (president) => president.party === "R" && president.terms === 1
+);
+console.table(republican);
 
 //3) return only the last three presidents
+const last3 = presidents.slice(9, 12);
+console.table(last3);
 
 //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+const dems2 = presidents.filter(
+  (president) => president.party === "D" && president.terms === 2
+);
+console.table(dems2);
 
 //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+//const LBJ = presidents.filter(
+// (president) => president.name === "Lyndon Johnson" && president.terms === 2
+//);
+//console.table(LBJ);
+/*const LBJ = presidents.filter(function (inventor) {
+  if (presidents.name === "Lyndon Johnson" && presidents.terms === 2);
+  {
+    return "LBJ served two terms";
+  }
+});
+console.table(LBJ);*/
+
+//const LBJ = presidents.some(function (president) {
+  ///const tru = "LBJ served two terms";
+  ///const fals = "LBJ was one and done";
+  const LJ = president.name === "Lyndon Johnson"{
+    if (president.terms === 2){
+      console.log("LBJ served two terms");
+    } else{
+      console.log("LBJ was one and done");
+    }
+  }
+  ///if (president.terms === 2 && president.name === "Lyndon Johnson") {
+  ///  console.log("LBJ served two terms");
+  ///} else {
+  ///  console.log("LBJ was one and done");
+ // }
+//});
+console.log(LBJ);
